38e9187070e9873b1eacf4f9750a003a156379e0
Saeedow <saeed320364@hotmail.com>
2024-01-08 20:20:57 +0100

    Tile and Flag

--- src/main/java/application/Tile.java
+++ src/main/java/application/Tile.java
@@ -1,5 +1,46 @@
 package application;
 
-public class Tile {
 
+import javafx.scene.image.*;
+
+import javafx.scene.canvas.GraphicsContext;
+
+public class Tile implements Collidable{
+	private Image image;
+	public double x;
+	public double y;
+	public int height;
+	public int width;
+
+	public Tile(Image image, double x, double y, int height, int width) {
+		this.image = image;
+		this.x = x;
+		this.y = y;
+		this.height = height;
+		this.width = width;
+	}
+
+	public void draw(GraphicsContext ctx) {
+		ctx.drawImage(image,x,y,height,width);
+	}
+
+	@Override
+	public double getX() {
+		return x;
+	}
+
+	@Override
+	public double getY() {
+		return y;
+	}
+
+	@Override
+	public int getwidth() {
+		return width;
+	}
+
+	@Override
+	public int getHeight() {
+		return height;
+	}
 }
