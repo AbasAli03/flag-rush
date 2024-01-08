71864ce0540fb2857ef9ad9dc6c6a6c410f0d8cc
AbasAli03 <125214662+AbasAli03@users.noreply.github.com>
2024-01-08 21:29:10 +0100

    bullet and bulletController

--- src/main/java/application/Bullet.java
+++ src/main/java/application/Bullet.java
@@ -1,5 +1,50 @@
 package application;
 
-public class Bullet {
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
 
+public class Bullet {
+	public int x;
+	public int y;
+	private int speed;
+	private int damage;
+	private int height;
+	private int width;
+	private  Color color;
+	
+	public Bullet(int x, int y, int speed, int damage) {
+		super();
+		this.x = x;
+		this.y = y;
+		this.speed = speed;
+		this.damage = damage;
+		this.width = 5;
+		this.height = 5;
+		this.color = Color.RED;
+	}
+	
+	public void draw(GraphicsContext ctx) {
+		ctx.setFill(this.color);
+		ctx.fillRect(x, y, width, height);
+	}
+	
+	public boolean isColliding(Collidable obj) {
+		
+		if (this.x < obj.getX() + obj.getwidth() &&
+			this.x + this.width > obj.getX() &&
+			this.y < obj.getY() + obj.getHeight() &&
+			this.y + this.height > obj.getY()) {
+			
+			if (obj instanceof Player) {
+				Player player = (Player) obj;	 
+				player.health -= this.damage;
+			}
+			
+			return true;
+		}
+		
+		return false;
+	}
+	
 }
+	
--- src/main/java/application/BulletController.java
+++ src/main/java/application/BulletController.java
@@ -1,5 +1,43 @@
 package application;
 
-public class BulletController {
+import java.util.List;
 
+import javafx.scene.canvas.GraphicsContext;
+
+public class BulletController {
+		
+	public List<Bullet> bullets;
+	private int time = 0;
+	
+	public BulletController() {
+			
+	}
+	
+	
+	public void shoot(int x, int y, int speed, int damage, int delay) {
+			if(this.time <= 0) {
+				this.bullets.add(new Bullet(x,y,speed,damage));
+				this.time = delay;
+			}
+			
+			this.time--;
+	}
+	
+	public void draw(GraphicsContext ctx) {
+		for(Bullet bullet : this.bullets) {
+			bullet.draw(ctx);
+		}
+	}
+	
+	public boolean isColliding(Collidable obj){
+		for(Bullet bullet : this.bullets) {
+			if(bullet.isColliding(obj)) {
+				this.bullets.remove(bullet);
+				return true;
+			}
+		}
+		
+		return false;
+		
+	}
 }
