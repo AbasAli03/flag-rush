1d4698754d15924a59842b3d1a92e7f7a1798974
AbasAli03 <125214662+AbasAli03@users.noreply.github.com>
2024-01-15 15:14:06 +0100

    fix

--- src/main/java/application/Flag.java
+++ src/main/java/application/Flag.java
@@ -1,6 +1,5 @@
 package application;
 
-
 import javafx.scene.canvas.*;
 
 import javafx.scene.image.*;
@@ -11,26 +10,26 @@ public class Flag {
 	private Image image = new Image("./assets/flag.png");
 	public int x;
 	public int y;
-	private int spawnX;
-	private int spawnY;
+	public int spawnX;
+	public int spawnY;
 	public int height;
 	public int width;
 	public boolean equiped = false;
-	
+
 	public Flag(int x, int y, int spawnX, int spawnY, int height, int width) {
 		this.x = x;
 		this.y = y;
-		this.spawnX = spawnX;
-		this.spawnY = spawnY;
+		this.x = spawnX;
+		this.y = spawnY;
 		this.height = height;
 		this.width = width;
 	}
 
+	public void restart() {
+		this.x = spawnX; 
+		
 
-	public void draw(GraphicsContext ctx) {
-			ctx.drawImage(image,x, y, width, height);
+
 	}
 	
-	
 }
-
